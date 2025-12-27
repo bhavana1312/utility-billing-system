70a746f76f536b98861ee797623e20ba2326086d
bhavana1312 <bhavana13sree@gmail.com>
2025-12-28 00:19:25 +0530

    modify pom

--- backend/discovery-server/src/main/java/com/utilitybilling/discoveryserver/DiscoveryServerApplication.java
+++ backend/discovery-server/src/main/java/com/utilitybilling/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.utilitybilling;
+package com.utilitybilling.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
