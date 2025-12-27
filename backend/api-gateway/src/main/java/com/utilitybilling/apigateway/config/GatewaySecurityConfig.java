cdab22fca39117576e2eb96dcd3ab6f659245b91
bhavana1312 <bhavana13sree@gmail.com>
2025-12-28 00:18:00 +0530

    add config

---
+++ backend/api-gateway/src/main/java/com/utilitybilling/apigateway/config/GatewaySecurityConfig.java
@@ -0,0 +1,19 @@
+package com.utilitybilling.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class GatewaySecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http.csrf(ServerHttpSecurity.CsrfSpec::disable).formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+				.httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+				.authorizeExchange(
+						exchange -> exchange.pathMatchers("/auth/**").permitAll().anyExchange().authenticated())
+				.build();
+	}
+}
